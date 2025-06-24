8a151ab6dbe298bd1177fbf5c3d6779c8c25d2a6
Stephen Isaac <stephenisaac9292@gmail.com>
2025-06-24 19:25:52 +0100

    save2

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -10,15 +10,14 @@ def stream_users_in_batches(batch_size):
         rows = cursor.fetchmany(batch_size)
         if not rows:
             break
-        yield [dict(row) for row in rows]
+        yield [dict(row) for row in rows]  # ✅ YIELD used
 
     cursor.close()
     conn.close()
 
 
 def batch_processing(batch_size):
-    for batch in stream_users_in_batches(batch_size):
-        # Filter users older than 25
-        filtered = (user for user in batch if user["age"] > 25)
-        for user in filtered:
-            print(user)
+    for batch in stream_users_in_batches(batch_size):  # 1st loop
+        for user in batch:                             # 2nd loop
+            if user["age"] > 25:
+                print(user)                            # ✅ Direct print, no return
