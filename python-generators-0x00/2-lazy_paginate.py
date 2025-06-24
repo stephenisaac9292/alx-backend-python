cc0912234bb6e523f237251d8d1577a074bee1b7
Stephen Isaac <stephenisaac9292@gmail.com>
2025-06-24 19:28:08 +0100

    save2

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,19 @@
+import seed  # as provided in the instruction
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+def lazy_pagination(page_size):
+    offset = 0
+    while True:  # only one loop
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page  # generator yielding one page at a time
+        offset += page_size
