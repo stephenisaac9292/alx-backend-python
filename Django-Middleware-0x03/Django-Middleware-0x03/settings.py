2d46688723c85d8442e3c0d961beaeb6f1e9d447
Stephen Isaac <stephenisaac9292@gmail.com>
2025-06-27 00:55:48 +0100

    hello

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -8,7 +8,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    "chats.middleware.OffensiveLanguageMiddleware"
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
